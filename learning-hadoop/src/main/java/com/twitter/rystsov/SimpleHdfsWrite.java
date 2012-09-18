7ff35c3edd6194a2ce34929989b55b43ea4db144
Denis Rystsov <rystsov.denis@gmail.com>
2012-09-19 01:39:27 +0400

    adds simple hdfs write test

---
+++ learning-hadoop/src/main/java/com/twitter/rystsov/SimpleHdfsWrite.java
@@ -0,0 +1,37 @@
+package com.twitter.rystsov;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class SimpleHdfsWrite
+{
+    public static void main( String[] args ) throws URISyntaxException, IOException {
+        String from = args[0];
+        String to =args[1];
+
+        InputStream src = new BufferedInputStream(new FileInputStream(from));
+
+        Configuration conf = new Configuration();
+        // Default replication factor is 3, so we
+        //   must change it to avoid under-replicated cluster state
+        // The following inequality must be true:
+        //   dfs.replication.min <= dfs.replication <= #datanodes
+        conf.set("dfs.replication","1");
+
+        FileSystem fs = FileSystem.get(new URI(to), conf);
+        FSDataOutputStream dst = fs.create(new Path(to), true);
+
+        // true => closes both src and dst
+        IOUtils.copyBytes(src, dst, 4096, true);
+    }
+}
