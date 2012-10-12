f6b599440f2599069658e0700169e5d2eb43f004
Denis Rystsov <rystsov.denis@gmail.com>
2012-10-12 18:41:13 +0400

    trying to add whole file input formatter

---
+++ src/main/java/com/twitter/rystsov/mr/input/WholeFileInputFormat.java
@@ -0,0 +1,29 @@
+package com.twitter.rystsov.mr.input;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.JobContext;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+
+import java.io.IOException;
+
+/**
+ * User: Denis Rystsov
+ */
+public class WholeFileInputFormat extends FileInputFormat<Text, BytesWritable> {
+
+
+    @Override
+    protected boolean isSplitable(JobContext context, Path file) {
+        return false;
+    }
+
+    @Override
+    public RecordReader<Text, BytesWritable> createRecordReader(InputSplit inputSplit, TaskAttemptContext taskAttemptContext) throws IOException, InterruptedException {
+        return new WholeFileRecordReader();
+    }
+}
